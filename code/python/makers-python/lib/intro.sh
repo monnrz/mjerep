309e5d64b9b1898466dce16ca80e331cea37d478
Matt East <99510489+monnrz@users.noreply.github.com>
2023-05-24 12:16:10 +0100

    Add files via upload

---
+++ code/python/makers-python/lib/intro.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BLUE='\033[1;34m'
+RED='\033[1;31m'
+NC='\033[0m'
+
+echo -e "${RED}If you're seeing scrolling text on the right,"
+echo "  you're in the wrong place."
+echo -e "Hit the 'Fork' button to get started.${NC}"
+echo
+echo -e "${BLUE}Otherwise: welcome home."
+echo "This is your shell."
+echo "You can use it to run Python programs."
+echo
+echo "If you ever lose it, hit the big green 'Run' button."
+echo
+echo "To run a test Python program, type:"
+echo -e "  ${NC}python lib/trial.py${BLUE}"
+echo
+echo -e "And then hit enter.${NC}"
+
